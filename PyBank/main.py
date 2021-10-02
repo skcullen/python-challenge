c0456b9497aee95a6f3f9b684d772ef4e515e6f8
Sarah Cullen <skcullen3.0@gmail.com>
2021-10-02 14:11:51 -0400

    commit 1

---
+++ PyBank/main.py
@@ -0,0 +1,70 @@
+# import needed modules
+import os
+import csv
+
+#create empty lists to store the data
+months = []
+profits = []
+profitchange = []
+
+#create path to needed data file
+csvpath = os.path.join('Resources','budget_data.csv')
+
+#set up to read csv file, skipping the header
+with open(csvpath, 'r') as budgetdata:
+    csvreader = csv.reader(budgetdata, delimiter=',')
+    csv_header = next(csvreader)
+
+    #set up code to loop through rows
+    for column in csvreader:
+
+        #fill in lists with column data, will ultimately use this to calclate sum and change in lists
+        months.append(column[0])
+        profits.append(int(column[1]))
+
+    #set up another loop to calculate the change for each row of profit
+    for x in range(len(profits)-1):
+
+        #calculate the change month to month
+        profitchange.append(profits[x+1]-profits[x])
+
+
+    #calculate average change
+    averagechange = sum(profitchange) / len(profitchange) 
+
+    #calculate min and max of profitchange
+    maxchange = max(profitchange)
+    minchange = min(profitchange)
+
+    #get the correlating month for the min and max
+    for x in range(len(profits)-1):
+        if profits[x+1] - profits[x] == maxchange:
+            maxmonth = months[x+1]
+        elif profits[x+1] - profits[x] == minchange:
+            minmonth = months[x+1]
+
+
+
+
+
+#create output data
+print("Financial Analysis")
+print("--------------------------------------")
+
+#get total number of months using the length function
+print(f"Total Months: {len(months)}")
+
+#total profit using sum of the profit list
+print(f"Total: ${sum(profits)}")
+
+#find average using list profit change
+print(f"Average Change: ${averagechange:.2f}")
+
+#greatest increase and decrease
+print(f"Greatest Increase in Profits: {maxmonth} (${maxchange})")
+print(f"Greatest Decrease in Profits: {minmonth} (${minchange})")
+
+
+
+    
+
---
+++ PyPoll/main.py
@@ -0,0 +1,104 @@
+# import needed modules
+import os
+import csv
+
+#create empty lists to store the data
+voters = []
+candidatevote = []
+canonevote = []
+cantwovote = []
+canthreevote = []
+canfourvote = []
+candidates = []
+percentlist = []
+countlist = []
+
+#create path to needed data file
+csvpath = os.path.join('Resources','election_data.csv')
+
+#set up to read csv file, skipping the header
+with open(csvpath, 'r') as electiondata:
+    csvreader = csv.reader(electiondata, delimiter=',')
+    csv_header = next(csvreader)
+
+    #set up code to loop through rows
+    for column in csvreader:
+
+        #fill in lists with column data, will ultimately use this to calclate sum of voters
+        voters.append(column[0])
+        candidatevote.append(column[2])
+
+    #create a list of candidates
+    candidatelistset = set(candidatevote)
+    #if running this code on new data, one must check how many candidates there are using the next line
+    #the data this was run on has 4 candidates, but you will need to make changes if that is different
+    #print(len(candidatelistset))
+
+    #covert set into list so that it can be indexed in next step
+    candidatelist = list(candidatelistset)
+
+    #put each candidate from candidate list into a loop which sorts them out intotheir own individual list by candidate
+    #if you have more or less candidates, you will have to edit the number of elifs, but the pattern is easily repeatable
+    #if changing number of candidates, remeber to add/drop new empty lists at top
+    for x in range(len(candidatevote)):
+        if candidatevote[x-1] == candidatelist[0]:
+            canonevote.append(candidatevote[x-1])
+        elif candidatevote[x-1] == candidatelist[1]:
+            cantwovote.append(candidatevote[x-1])
+        elif candidatevote[x-1] == candidatelist[2]:
+            canthreevote.append(candidatevote[x-1])
+        elif candidatevote[x-1] == candidatelist[3]:
+            canfourvote.append(candidatevote[x-1])
+
+    #find percent of vote for each candidate (the percent sign will be added later)
+    canonepercent = (len(canonevote)/len(voters))*100
+    cantwopercent = (len(cantwovote)/len(voters))*100
+    canthreepercent = (len(canthreevote)/len(voters))*100
+    canfourpercent = (len(canfourvote)/len(voters))*100
+
+    #find vote count for each candidate
+    canonecount = (len(canonevote))
+    cantwocount = (len(cantwovote))
+    canthreecount = (len(canthreevote))
+    canfourcount = (len(canfourvote))
+    
+    #put percentages into list
+    percentlist.append(canonepercent)
+    percentlist.append(cantwopercent)
+    percentlist.append(canthreepercent)
+    percentlist.append(canfourpercent)
+
+    #put vote count into list
+    countlist.append(canonecount)
+    countlist.append(cantwocount)
+    countlist.append(canthreecount)
+    countlist.append(canfourcount)
+
+    #use max of countlist or percentlist to find the winner
+    if max(countlist) == canonecount:
+        winner = candidatelist[0]
+    elif max(countlist) == cantwocount:
+        winner = candidatelist[1]
+    elif max(countlist) == canthreecount:
+        winner = candidatelist[2]
+    elif max(countlist) == canfourcount:
+        winner = candidatelist[3]
+
+#create output data
+print(f"Election Results")
+print("-----------------------------")
+
+#find sum of voters and print
+print(f"Total Votes: {len(voters)}")
+print("-----------------------------")
+
+#candidate, percentage and vote count
+print(f"{candidatelist[0]}: {canonepercent:.3f}% ({canonecount})") 
+print(f"{candidatelist[1]}: {cantwopercent:.3f}% ({cantwocount})")
+print(f"{candidatelist[2]}: {canthreepercent:.3f}% ({canthreecount})")
+print(f"{candidatelist[3]}: {canfourpercent:.3f}% ({canfourcount})")
+print("-----------------------------")
+
+#print the winner
+print(f"Winner: {winner}")
+print("-----------------------------")
